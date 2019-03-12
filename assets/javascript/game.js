cba19731615503d7ef593703f762019738d17355
JLindley <jonlindley@gmail.com>
2019-03-12 16:14:34 -0700

    starbucks is goood

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// words
+
+var word = ["Hermione", "Draco", "Hagrid", "Sirius", "Ginny", "Minerva", "Newt", "Neville", "Peter", "Cho", "Horace"]
+
+//choose word randomly
+var randNum = Math.floor(math.random() * word.length);
+var choosenWord = word[randNum];
+console.log(chosenWord)
+
+//create underscores based on length of word
+generateUnderscore = () => {
+    for(let i = 0; i < chosenWord.length; i++) 
+}
+//Get users guess
+
+//check if guess is right
+
+//if right push to right array
+
+//if wrong push to wrong array
+
+//
